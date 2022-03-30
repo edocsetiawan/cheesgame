6b8b2cbd7478d81079897f601d30a388254452cc
Edo C. Setiawan <edocsetiawan@gmail.com>
2022-03-30 07:35:55 +0700

    add barier

--- main.php
+++ main.php
@@ -6,8 +6,13 @@
         echo 'masukan bidak '.$i.'(x,y)'.PHP_EOL;
         $input = fopen("php://stdin","r");
         $value = fgets($input);
-        $move[$i] = strval($value);
-        $move = array_map('trim', $move);
+        if(!strpos($value,',')){
+            echo 'wrong format!'.PHP_EOL;
+            $i--;
+        }else{
+            $move[$i] = strval($value);
+            $move = array_map('trim', $move);
+        }
     }
     echo 'hasil'.PHP_EOL;
     $result = $checkPawn->findPawn($move);
